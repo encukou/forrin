db24bc8c35c3ccbd7357458e08dfeca57ac5f549
Petr Viktorin <encukou@gmail.com>
2012-06-29 00:11:11 +0200

    Set version to 0.1-alpha, require polib 1.0

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='forrin',
-    version='0.1',
+    version='0.1-alpha',
     packages=find_packages(),
 
     description=u"""Localization helpers""",
     author='Petr Viktorin',
     author_email='encukou@gmail.com',
     install_requires=[
-            'polib>=0.5',
+            'polib>=1.0',
         ],
 
     entry_points = {
